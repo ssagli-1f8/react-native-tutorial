a32655b3a4c83d6c56edfb7163357d92e9ec60e7
Sawako Sagliano <ssagli@1f8.dev>
2021-02-19 16:40:48 +0900

    adding counter

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const ColorScreen = () => {
+    return (
+        <View>
+            <Button
+            title='Add a Color'
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ColorScreen
