1fe732831f23ee6b3fa04a29523cc0f7a4f28123
Sawako Sagliano <ssagli@1f8.dev>
2021-03-05 14:19:45 +0900

    square screen, reducer and states

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -27,6 +27,10 @@ const HomeScreen = ({ navigation }) => {
         title='Go to Color Demo'
         onPress={() => navigation.navigate('Color')}
       />
+      <Button 
+        title='Square Screen Demo'
+        onPress={() => navigation.navigate('Square')}
+      />
     </View>
     )
 };
